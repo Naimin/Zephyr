35016cfdd370fa30e30ed4f77ed4f4cb84caa6cb
Koh Naimin <Koh Naimin>
2017-12-13 20:55:43 +0800

    Fix the openmesh decimation module not properly garbage collecting. Fix issue with the decimation module having no constraint hence completely decimate any model depite the target face count.

--- Zephyr_Common/src/Mesh/OM_Mesh.cpp
+++ Zephyr_Common/src/Mesh/OM_Mesh.cpp
@@ -2,6 +2,8 @@
 #include <tbb/parallel_for.h>
 
 #include <OpenMesh/Tools/Decimater/ModQuadricT.hh>
+#include <OpenMesh/Tools/Decimater/ModNormalFlippingT.hh>
+#include <OpenMesh/Tools/Decimater/ModNormalDeviationT.hh>
 #include <OpenMesh/Tools/Decimater/DecimaterT.hh>
 #include <OpenMesh/Tools/Decimater/ModBaseT.hh>
 
@@ -15,6 +17,8 @@ using namespace Zephyr::Common;
 // Decimater type
 typedef Decimater::DecimaterT< Zephyr::Common::OMMesh > Decimator;
 typedef Decimater::ModQuadricT< Zephyr::Common::OMMesh >::Handle HModQuadric;
+typedef Decimater::ModNormalFlippingT< Zephyr::Common::OMMesh >::Handle HModNormalFlipping;
+typedef Decimater::ModNormalDeviationT< Zephyr::Common::OMMesh >::Handle HModNormalDeviation;
 
 Zephyr::Common::OpenMeshMesh::OpenMeshMesh()
 {
@@ -66,10 +70,6 @@ void Zephyr::Common::OpenMeshMesh::loadMesh(Common::Mesh & mesh)
 
 		mMesh.add_face(vertexHandles[0], vertexHandles[1], vertexHandles[2]);
 	}
-
-	auto edgeCount = mMesh.n_edges();
-	auto halfEdgeCount = mMesh.n_halfedges();
-
 }
 
 OMMesh& Zephyr::Common::OpenMeshMesh::getMesh()
@@ -95,47 +95,57 @@ int Zephyr::Common::OpenMeshMesh::decimate(unsigned int targetFaceCount, Decimat
 	std::cout << "Using ";
 	if (RANDOM_DECIMATE == type)
 	{
-		std::cout << "Random Decimation...";
+		std::cout << "Random Decimation..." << std::endl;
 		collapseCount = decimateRandom(targetFaceCount);
 	}
 	else
 	{
-		std::cout << "Greedy Decimation...";
+		std::cout << "Greedy Decimation..." << std::endl;
 		collapseCount = decimateGreedy(targetFaceCount);
 	}
-	mMesh.garbage_collection();
 	auto elapseTime = timer.getElapsedTime();
 
-	std::cout << "Done in " << elapseTime << " sec" << std::endl;
+	std::cout << "Decimation done in " << elapseTime << " sec" << std::endl;
 	std::cout << "Original Face Count: " << previousFaceCount << std::endl;
 	std::cout << "Target Face Count: " << targetFaceCount << std::endl;
+	std::cout << "Removed Face Count: " << collapseCount << std::endl;
 	std::cout << "Decimated Face Count: " << mMesh.n_faces() << std::endl;
-	std::cout << "Percentage decimated: " << mMesh.n_faces() / (float)previousFaceCount << " %" << std::endl;
+	std::cout << "Percentage decimated: " << ((previousFaceCount - mMesh.n_faces()) / (float)previousFaceCount) * 100.0f << " %" << std::endl;
 
-	exports("D:\\sandbox\\decimatedMesh.obj");
+	//exports("D:\\sandbox\\decimatedMesh.obj");
 
 	return collapseCount;
 }
 
 int Zephyr::Common::OpenMeshMesh::decimateGreedy(unsigned int targetFaceCount)
 {
-	Decimator decimator(mMesh);
-	HModQuadric hModQuadric;      // use a quadric module
+	const float maxQuadricError = 0.001f;
+	const float maxNormalFlipDeviation = 45.0f;
+	const float maxNormalDeviation = 15.0f;
 
-	decimator.add(hModQuadric); // register module at the decimater
+	Decimator decimator(mMesh);
 	
-	/*
-	* since we need exactly one priority module (non-binary)
-	* we have to call set_binary(false) for our priority module
-	* in the case of HModQuadric, unset_max_err() calls set_binary(false) internally
-	*/
-	//
-	//decimator.module(hModQuadric).unset_max_err();
-	decimator.initialize();
+	HModQuadric hModQuadric;      // use a quadric module
+	decimator.add(hModQuadric); // register module at the decimator
+	decimator.module(hModQuadric).set_max_err(maxQuadricError);
+	
+	HModNormalFlipping hModNormalFlipping; // use a module that prevent flipping of triangle
+	decimator.add(hModNormalFlipping); 					// register the normal flipping module
+	decimator.module(hModNormalFlipping).set_max_normal_deviation(maxNormalFlipDeviation);	// set the maximum normal deviation 
+
+	HModNormalDeviation hModNormalDeviation; // use a module that prevent large change in face normal
+	decimator.add(hModNormalDeviation); // register module to the decimator
+	decimator.module(hModNormalDeviation).set_binary(true); // set to true if not the main decimation score
+	decimator.module(hModNormalDeviation).set_normal_deviation(maxNormalDeviation);
+
+	if (!decimator.initialize())
+		return -1;
 
 	size_t numOfCollapseRequired = mMesh.n_faces() - targetFaceCount;
 
 	size_t actualNumOfCollapse = decimator.decimate(numOfCollapseRequired);
+
+	mMesh.garbage_collection();
 
 	return (int)actualNumOfCollapse;
 }
